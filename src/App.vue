66d80ba72da9a5527a90b2e60e6972eab30fd062
ajcm <ajcm2020@proton.me>
2025-05-23 09:16:39 +0100

    add auth

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+import Test from './components/Test.vue'
 </script>
 
 <template>
   <main>
+    <Test />
     <Todos />
   </main>
 </template>
